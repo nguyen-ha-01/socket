4f232d26d7a06a67e5ee54274fe0dece03083e0f
nguyen-ha-01 <78808937+nguyen-ha-01@users.noreply.github.com>
2024-03-10 16:48:45 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-const { messaging } = require("firebase-admin");
 const ws = require("ws");
+curentUser = 0;
 const server = new ws.Server({port:8080});
 server.on("connection",(ws)=>{
-    console.log("connected");
+    
+    curentUser+=1;
+    console.log("connected current user is"+ curentUser);
     ws.on("message", (message)=>{
-        ws.send(message);
+        ws.send(message.toString());
     });
 } );
+server.on("disconnection",(ws)=>{
+    curentUser-=1;
+});
